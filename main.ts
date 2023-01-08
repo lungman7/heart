abc1bdd049c72588e5e8958064222906d08d4303
lungman7 <122063812+lungman7@users.noreply.github.com>
2023-01-08 23:20:44 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,8 @@
+basic.showIcon(IconNames.Happy)
+basic.pause(100)
+basic.forever(function () {
+    basic.showIcon(IconNames.Heart)
+    basic.pause(100)
+    basic.showIcon(IconNames.SmallHeart)
+    basic.pause(100)
+})
